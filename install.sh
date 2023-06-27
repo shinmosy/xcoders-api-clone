12dbd684ec9fc1fdaf3650059c610ec56def2b93
Ryumosy <114341726+shinmosy@users.noreply.github.com>
2023-06-28 01:57:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-git config --global user.email "farhanart010503@gmail.com"
-git config --global user.name "FarhannnnX"
+git config --global user.email "mosyafik.jr@gmail.com"
+git config --global user.name "shinmosy"
 git init
 git add *
 git commit -m "new update"
 git branch -M master
-git remote add origin https://github.com/FarhannnnX/restAPIs.git
+git remote add origin https://github.com/shinmosy/restAPIs.git
 git push -u origin master
